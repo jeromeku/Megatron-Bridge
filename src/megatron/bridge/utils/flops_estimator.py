df824dde981c9b03e18e5a0e60d3938a7062e29a
jeromeku <jerome.ku@gmail.com>
2025-11-09 01:32:26 +0000

    add model flops check

--- src/megatron/bridge/utils/flops_estimator.py
+++ src/megatron/bridge/utils/flops_estimator.py
@@ -1,34 +1,48 @@
-import argparse
-from transformers import AutoConfig
-import sys, types
+# ruff: noqa
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
-# # stub out wandb before importing megatron
-# def stub_import(module: str):
-#     stub = types.ModuleType("stub")
-#     stub.init = lambda *a, **k: None
-#     sys.modules[module] = stub
-# stub_import("wandb")
-# stub_import("transformer_engine")
-# stub_import("transformer_engine.common")
-# stub_import("transformer_engine.pytorch")
-# stub_import("transformer_engine.pytorch.tensor")
+import argparse
+from transformers import AutoConfig, AutoModelForCausalLM
+import torch
 
 from megatron.bridge import AutoBridge
-from megatron.bridge.training.config import ConfigContainer
 from megatron.bridge.training.utils import flop_utils
 
+NEMOTRON_9B_v2 = "nvidia/NVIDIA-Nemotron-Nano-9B-v2"
+
+def _estimate_model_flops(model: torch.nn.Module):
+    num_params = sum(p.numel() for p in model.parameters())
+
+    return 6 * num_params
+
+def estimate_model_flops(model: torch.nn.Module, units: str = "G"):
+    flops = _estimate_model_flops(model)
+    match units:
+        case "G": 
+            return flops / 1e9
+        case "M":
+            return flops / 1e6
+        case _:
+            return flops
+        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_id", type=str, default="Qwen/Qwen3-1.7B")
+    parser.add_argument("--no-check", action="store_false", dest="check")
     args = parser.parse_args()
     
-    hf_config = AutoConfig.from_pretrained(args.model_id)
-    bridge = AutoBridge.from_hf_pretrained(args.model_id)
-    print(bridge)
+    hf_config = AutoConfig.from_pretrained(args.model_id, trust_remote_code=True)
+    
+    bridge = AutoBridge.from_hf_pretrained(args.model_id, trust_remote_code=True)
     model_cfg = bridge.to_megatron_provider(load_weights=False)
-    cfg = ConfigContainer(model=model_cfg)
 
-    flops = flop_utils.num_floating_point_operations(cfg, batch_size=1)
+    flops = flop_utils.num_floating_point_operations(model_config=model_cfg, batch_size=1)
     gflops_per_seq = flops / 1e9
     gflops_per_token = gflops_per_seq / model_cfg.seq_length
-    print(f"{args.model_id}: {gflops_per_token} GFlops")  
+    print(f"{args.model_id}: {gflops_per_token:.1f} GFlops per token")
+    breakpoint()
+    if args.check:
+        with torch.device("meta"):
+            hf_model = AutoModelForCausalLM.from_config(hf_config)
+            print(f"Model flops estimate: {estimate_model_flops(hf_model, units='G'):.1f} GFlops per token")
